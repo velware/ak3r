88c9a85047130b3d528dc25fcd5381a16f30de8c
Hector U. Velez <huvelez@gmail.com>
2018-01-11 19:37:55 -0500

    Initial Server Upload

--- src/main/java/com/velware/ak3r/NioServer.java
+++ src/main/java/com/velware/ak3r/NioServer.java
@@ -6,13 +6,11 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
 
 public class NioServer {
 
-    private static final String POISON_PILL = "POISON_PILL";
     private ServerSocketChannel channel;
     private Selector selector;
     private String hostname;
